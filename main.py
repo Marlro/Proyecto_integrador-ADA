6d8a19d4a404ee6d6c7bcb2f88c03360f70d9ac9
Marleni Rodriguez <rodriguezmarleni1523@gmail.com>
2023-10-24 18:58:38 -0600

    Parte 4

--- main.py
+++ main.py
@@ -38,3 +38,56 @@ def main():
 
 if __name__ == "__main__":
     main()
+def convertir_mapa_a_matriz(mapa):
+    matriz = [list(fila) for fila in mapa.split("\n")]
+    return matriz
+
+def mostrar_mapa(mapa):
+    for fila in mapa:
+        print("".join(fila))
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main_loop(mapa, posicion_inicial, posicion_final):
+    px, py = posicion_inicial
+    
+    while (px, py) != posicion_final:
+        mapa[py][px] = 'P'
+        limpiar_pantalla()
+        mostrar_mapa(mapa)
+
+        # Leer teclas y procesar movimiento
+        key = readchar.readkey()
+        nueva_px, nueva_py = px, py
+
+        if key == '\x1b[A':  # Flecha arriba
+            nueva_py = max(0, py - 1)
+        elif key == '\x1b[B':  # Flecha abajo
+            nueva_py = min(len(mapa) - 1, py + 1)
+        elif key == '\x1b[C':  # Flecha derecha
+            nueva_px = min(len(mapa[0]) - 1, px + 1)
+        elif key == '\x1b[D':  # Flecha izquierda
+            nueva_px = max(0, px - 1)
+        
+        if mapa[nueva_py][nueva_px] != '#':
+            mapa[py][px] = '.'
+            px, py = nueva_px, nueva_py
+
+# Ejemplo de uso
+mapa = """
+##########
+#........#
+#.#.######
+#.#.#....#
+#.#.#.##.#
+#...#....#
+########.#
+#........#
+########.#
+"""
+mapa_matriz = convertir_mapa_a_matriz(mapa)
+posicion_inicial = (0, 0)
+posicion_final = (9, 9)
+
+main_loop(mapa_matriz, posicion_inicial, posicion_final)
